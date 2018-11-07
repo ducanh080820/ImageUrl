95ef77f70e1a6301b709fe16e5c51c50745ba097
Trần Đức Anh <ducanhxom4@gmail.com>
2018-11-07 07:32:09 +0700

    add

--- ImageUrl/ViewController.swift
+++ ImageUrl/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    let catPictureURl = URL(string: "http://i.imgur.com/w5rkSIj.jpg")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let session = URLSession(configuration: .default)
+        let downloadPicTask = session.dataTask(with: catPictureURl) { (data, respone, error) in
+            if let e = error {
+                print("Error Occurred: \(e)")
+            } else {
+                if (respone as? HTTPURLResponse) != nil {
+                    if let imageData = data {
+                        let image = UIImage(data: imageData)
+                        self.myImageView.image = image
+                    } else {
+                        print("Image file is currupted")
+                    }
+                } else {
+                    print("No respone from server")
+                }
+            }
+        }
+        downloadPicTask.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
